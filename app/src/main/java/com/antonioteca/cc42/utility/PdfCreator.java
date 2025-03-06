24b07c1ad102285d51fce3d5a170c345bb980e56
antoniotecadev <antonioteca@hotmail.com>
2025-03-06 16:14:35 +0100

    simplicar código

--- app/src/main/java/com/antonioteca/cc42/utility/PdfCreator.java
+++ app/src/main/java/com/antonioteca/cc42/utility/PdfCreator.java
@@ -71,7 +71,7 @@ public class PdfCreator {
             Document document = new Document(pdf, PageSize.A4);
             document.setMargins(20, 20, 20, 20);
             // Evento marca d'agua
-            pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new ImageWatermarkEvent(getImageDataFromDrawable(context, R.drawable.check_cadet_logotipo)));
+            pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new ImageWatermarkEvent(getImageDataFromDrawable(context)));
             // Rodapé
             pdf.addEventHandler(PdfDocumentEvent.END_PAGE, event -> {
                 PdfDocumentEvent documentEvent = (PdfDocumentEvent) event;
@@ -149,8 +149,8 @@ public class PdfCreator {
         }
     }
 
-    private static ImageData getImageDataFromDrawable(Context context, int drawebleId) {
-        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), drawebleId);
+    private static ImageData getImageDataFromDrawable(Context context) {
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.check_cadet_logotipo);
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
         byte[] byteArray = stream.toByteArray();
@@ -170,7 +170,7 @@ public class PdfCreator {
             Document document = new Document(pdf, PageSize.A4);
             document.setMargins(20, 20, 20, 20);
             // Evento marca d'agua
-            pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new ImageWatermarkEvent(getImageDataFromDrawable(context, R.drawable.check_cadet_logotipo)));
+            pdf.addEventHandler(PdfDocumentEvent.END_PAGE, new ImageWatermarkEvent(getImageDataFromDrawable(context)));
             // Rodapé
             pdf.addEventHandler(PdfDocumentEvent.END_PAGE, event -> {
                 PdfDocumentEvent documentEvent = (PdfDocumentEvent) event;
