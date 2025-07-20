8adb56a71ab47a814b94720e1f4a1265a342cf7d
António Teca <antonioteca@hotmail.com>
2025-07-21 00:40:22 +0100

    Comentar código relacionado ao reconhecimento facial.
    Esta alteração comenta o código relacionado à funcionalidade de reconhecimento facial, incluindo:
    - Arquivos Java: `FaceDetectionHelper`, `FaceRecognitionHelper`, `FaceRecognitionFirebaseUtils` e `FaceRecognitionFragment`.
    - Arquivos C++: `yuv_utils.cpp`, `face_matcher.cpp` e `CMakeLists.txt`.
    - Configurações do Gradle: remoção de dependências e configurações relacionadas ao NDK e AAPT.
    
    A funcionalidade de reconhecimento facial está temporariamente desabilitada.

--- app/src/main/java/com/antonioteca/cc42/utility/FaceRecognitionFirebaseUtils.java
+++ app/src/main/java/com/antonioteca/cc42/utility/FaceRecognitionFirebaseUtils.java
@@ -1,85 +1,85 @@
-package com.antonioteca.cc42.utility;
-
-import android.content.Context;
-import android.view.View;
-import android.widget.ProgressBar;
-
-import androidx.annotation.NonNull;
-
-import com.antonioteca.cc42.viewmodel.SharedViewModel;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class FaceRecognitionFirebaseUtils {
-    private final DatabaseReference databaseRef;
-
-    public FaceRecognitionFirebaseUtils(String campusId, String cursusId) {
-        databaseRef = FirebaseDatabase.getInstance().getReference("campus")
-                .child(campusId)
-                .child("cursus")
-                .child(cursusId)
-                .child("face_embeddings");
-    }
-
-    public void saveEmbedding(String userId, float[] embedding, SharedViewModel sharedViewModel, ProgressBar progressBar, Context context) {
-        Map<String, Object> data = new HashMap<>();
-        data.put("embedding", convertEmbeddingForList(embedding));
-
-        databaseRef.child(userId).setValue(data)
-                .addOnSuccessListener(unused -> {
-                    progressBar.setVisibility(View.INVISIBLE);
-                    Util.showAlertDialogBuild("Sucesso", "Face ID registado com sucesso", context, () ->
-                            sharedViewModel.setUserFaceIdContinueCaptureLiveData(true)
-                    );
-                })
-                .addOnFailureListener(e -> {
-                    progressBar.setVisibility(View.INVISIBLE);
-                    Util.showAlertDialogBuild("Erro", e.getMessage(), context, () ->
-                            sharedViewModel.setUserFaceIdContinueCaptureLiveData(true)
-                    );
-                });
-    }
-
-    public void getAllEmbeddings(ValueEventListener listener) {
-        databaseRef.addListenerForSingleValueEvent(listener);
-    }
-
-    @NonNull
-    private List<Float> convertEmbeddingForList(@NonNull float[] embedding) {
-        List<Float> list = new ArrayList<>();
-        for (float value : embedding) list.add(value);
-        return list;
-    }
-
-    @NonNull
-    public float[][] convertListForEmbedding(@NonNull List<Object> embeddinList) {
-        float[][] embedding = new float[embeddinList.size()][192];
-        if (!embeddinList.isEmpty() && embeddinList.get(0) instanceof Map) {
-            for (int i = 0; i < embeddinList.size(); i++) {
-                Map<String, Object> userMap = (Map<String, Object>) embeddinList.get(i);
-                if (userMap.containsKey("embedding")) {
-                    List<Object> row = (List<Object>) userMap.get("embedding");
-                    if (row != null) {
-                        for (int j = 0; j < row.size(); j++) {
-                            Object valueInRow = row.get(j);
-                            if (valueInRow instanceof Double) {
-                                embedding[i][j] = ((Double) valueInRow).floatValue();
-                            } else if (valueInRow instanceof Float) {
-                                embedding[i][j] = (Float) valueInRow;
-                            } else {
-                                embedding[i][j] = 0.0f;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return embedding;
-    }
-}
+//package com.antonioteca.cc42.utility;
+//
+//import android.content.Context;
+//import android.view.View;
+//import android.widget.ProgressBar;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.antonioteca.cc42.viewmodel.SharedViewModel;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class FaceRecognitionFirebaseUtils {
+//    private final DatabaseReference databaseRef;
+//
+//    public FaceRecognitionFirebaseUtils(String campusId, String cursusId) {
+//        databaseRef = FirebaseDatabase.getInstance().getReference("campus")
+//                .child(campusId)
+//                .child("cursus")
+//                .child(cursusId)
+//                .child("face_embeddings");
+//    }
+//
+//    public void saveEmbedding(String userId, float[] embedding, SharedViewModel sharedViewModel, ProgressBar progressBar, Context context) {
+//        Map<String, Object> data = new HashMap<>();
+//        data.put("embedding", convertEmbeddingForList(embedding));
+//
+//        databaseRef.child(userId).setValue(data)
+//                .addOnSuccessListener(unused -> {
+//                    progressBar.setVisibility(View.INVISIBLE);
+//                    Util.showAlertDialogBuild("Sucesso", "Face ID registado com sucesso", context, () ->
+//                            sharedViewModel.setUserFaceIdContinueCaptureLiveData(true)
+//                    );
+//                })
+//                .addOnFailureListener(e -> {
+//                    progressBar.setVisibility(View.INVISIBLE);
+//                    Util.showAlertDialogBuild("Erro", e.getMessage(), context, () ->
+//                            sharedViewModel.setUserFaceIdContinueCaptureLiveData(true)
+//                    );
+//                });
+//    }
+//
+//    public void getAllEmbeddings(ValueEventListener listener) {
+//        databaseRef.addListenerForSingleValueEvent(listener);
+//    }
+//
+//    @NonNull
+//    private List<Float> convertEmbeddingForList(@NonNull float[] embedding) {
+//        List<Float> list = new ArrayList<>();
+//        for (float value : embedding) list.add(value);
+//        return list;
+//    }
+//
+//    @NonNull
+//    public float[][] convertListForEmbedding(@NonNull List<Object> embeddinList) {
+//        float[][] embedding = new float[embeddinList.size()][192];
+//        if (!embeddinList.isEmpty() && embeddinList.get(0) instanceof Map) {
+//            for (int i = 0; i < embeddinList.size(); i++) {
+//                Map<String, Object> userMap = (Map<String, Object>) embeddinList.get(i);
+//                if (userMap.containsKey("embedding")) {
+//                    List<Object> row = (List<Object>) userMap.get("embedding");
+//                    if (row != null) {
+//                        for (int j = 0; j < row.size(); j++) {
+//                            Object valueInRow = row.get(j);
+//                            if (valueInRow instanceof Double) {
+//                                embedding[i][j] = ((Double) valueInRow).floatValue();
+//                            } else if (valueInRow instanceof Float) {
+//                                embedding[i][j] = (Float) valueInRow;
+//                            } else {
+//                                embedding[i][j] = 0.0f;
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        return embedding;
+//    }
+//}
--- app/src/main/java/com/antonioteca/cc42/utility/FaceRecognitionHelper.java
+++ app/src/main/java/com/antonioteca/cc42/utility/FaceRecognitionHelper.java
@@ -1,50 +1,50 @@
-package com.antonioteca.cc42.utility;
-
-import android.content.Context;
-import android.graphics.Bitmap;
-
-import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.support.common.FileUtil;
-
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.MappedByteBuffer;
-
-public class FaceRecognitionHelper {
-
-    static {
-        System.loadLibrary("face_matcher");
-    }
-
-    // Chamada nativa
-    public static native int matchSIMD(float[] embedding1, float[][] embedding2, float threshold);
-
-    private final Interpreter interpreter; // Interpretador do TensorFlow Lite
-
-    public FaceRecognitionHelper(Context context) throws IOException {
-        MappedByteBuffer modelBuffer = FileUtil.loadMappedFile(context, "mobilefacenet.tflite");
-        interpreter = new Interpreter(modelBuffer);
-    }
-
-    // Método para obter o embedding (vectores de características) de uma face
-    public float[] getFaceEmbedding(Bitmap faceBitmap) {
-        Bitmap resized = Bitmap.createScaledBitmap(faceBitmap, 112, 112, true); // Redimensionar a imagem para o tamanho esperado pelo modelo
-        ByteBuffer inputBuffer = ByteBuffer.allocateDirect(112 * 112 * 3 * 4); // Buffer para armazenar os dados de entrada
-        inputBuffer.order(ByteOrder.nativeOrder()); // Definir a ordem dos bytes na CPU
-
-        int[] intValues = new int[112 * 112]; // Array para armazenar os valores RGB dos pixels
-        resized.getPixels(intValues, 0, 112, 0, 0, 112, 112); // Obter os valores RGB dos pixels da imagem redimensionada
-
-        // Preencher o buffer com os valores RGB normalizados
-        for (int val : intValues) {
-            inputBuffer.putFloat(((val >> 16) & 0xFF) / 255.0f);
-            inputBuffer.putFloat(((val >> 8) & 0xFF) / 255.0f);
-            inputBuffer.putFloat((val & 0xFF) / 255.0f);
-        }
-
-        float[][] embedding = new float[1][192]; // Buffer para armazenar o embedding (vetores de características)
-        interpreter.run(inputBuffer, embedding); // Executar a inferência no modelo TensorFlow Lite
-        return embedding[0]; // Retornar o embedding (vetor de características)
-    }
-}
+//package com.antonioteca.cc42.utility;
+//
+//import android.content.Context;
+//import android.graphics.Bitmap;
+//
+//import org.tensorflow.lite.Interpreter;
+//import org.tensorflow.lite.support.common.FileUtil;
+//
+//import java.io.IOException;
+//import java.nio.ByteBuffer;
+//import java.nio.ByteOrder;
+//import java.nio.MappedByteBuffer;
+//
+//public class FaceRecognitionHelper {
+//
+//    static {
+//        System.loadLibrary("face_matcher");
+//    }
+//
+//    // Chamada nativa
+//    public static native int matchSIMD(float[] embedding1, float[][] embedding2, float threshold);
+//
+//    private final Interpreter interpreter; // Interpretador do TensorFlow Lite
+//
+//    public FaceRecognitionHelper(Context context) throws IOException {
+//        MappedByteBuffer modelBuffer = FileUtil.loadMappedFile(context, "mobilefacenet.tflite");
+//        interpreter = new Interpreter(modelBuffer);
+//    }
+//
+//    // Método para obter o embedding (vectores de características) de uma face
+//    public float[] getFaceEmbedding(Bitmap faceBitmap) {
+//        Bitmap resized = Bitmap.createScaledBitmap(faceBitmap, 112, 112, true); // Redimensionar a imagem para o tamanho esperado pelo modelo
+//        ByteBuffer inputBuffer = ByteBuffer.allocateDirect(112 * 112 * 3 * 4); // Buffer para armazenar os dados de entrada
+//        inputBuffer.order(ByteOrder.nativeOrder()); // Definir a ordem dos bytes na CPU
+//
+//        int[] intValues = new int[112 * 112]; // Array para armazenar os valores RGB dos pixels
+//        resized.getPixels(intValues, 0, 112, 0, 0, 112, 112); // Obter os valores RGB dos pixels da imagem redimensionada
+//
+//        // Preencher o buffer com os valores RGB normalizados
+//        for (int val : intValues) {
+//            inputBuffer.putFloat(((val >> 16) & 0xFF) / 255.0f);
+//            inputBuffer.putFloat(((val >> 8) & 0xFF) / 255.0f);
+//            inputBuffer.putFloat((val & 0xFF) / 255.0f);
+//        }
+//
+//        float[][] embedding = new float[1][192]; // Buffer para armazenar o embedding (vetores de características)
+//        interpreter.run(inputBuffer, embedding); // Executar a inferência no modelo TensorFlow Lite
+//        return embedding[0]; // Retornar o embedding (vetor de características)
+//    }
+//}
