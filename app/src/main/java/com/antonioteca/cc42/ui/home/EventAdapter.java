c8c4f76df5ef2b6d9cdc92c64d6c4807cce04322
António Teca <antonioteca@hotmail.com>
2025-08-28 10:21:51 +0100

    Salvar estado do filtro de eventos encerrados.
    O estado do filtro de eventos encerrados agora é salvo em SharedPreferences,
    permitindo que a preferência do usuário seja mantida entre as sessões do
    aplicativo. Ao reabrir a lista de eventos, o filtro será aplicado
    automaticamente conforme a última seleção.

--- app/src/main/java/com/antonioteca/cc42/ui/home/EventAdapter.java
+++ app/src/main/java/com/antonioteca/cc42/ui/home/EventAdapter.java
@@ -1,8 +1,10 @@
 package com.antonioteca.cc42.ui.home;
 
+import static android.content.Context.MODE_PRIVATE;
 import static com.antonioteca.cc42.utility.Util.dpToPx;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
@@ -28,11 +30,13 @@ import java.util.TimeZone;
 
 public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private Context context;
+    private final Context context;
     private final String colorCoalition;
+
+    private final SharedPreferences.Editor editor;
     private final List<Event> eventList = new ArrayList<>();
     private final List<Event> eventListEnd = new ArrayList<>();
-    private boolean showEventListEnd = false;
+    private boolean showEventListEnd;
     private final SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault());
     private final SimpleDateFormat outputFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.getDefault());
 
@@ -48,7 +52,12 @@ public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
         TYPE_FOOTER = 1;
     }
 
-    public EventAdapter(@NonNull List<Event> eventList, String colorCoalition, Context context) {
+    public EventAdapter(@NonNull List<Event> eventList, String colorCoalition, @NonNull Context context) {
+
+        SharedPreferences preferences = context.getSharedPreferences("MyAppPrefsEvent", MODE_PRIVATE);
+        editor = preferences.edit();
+
+        this.showEventListEnd = preferences.getBoolean("showEventListEnd", false);
 
         OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
 
@@ -59,6 +68,11 @@ public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
             else
                 this.eventListEnd.add(event);
         }
+
+        if (this.showEventListEnd) {
+            this.eventList.addAll(this.eventListEnd);
+        }
+
         this.colorCoalition = colorCoalition;
         this.context = context;
     }
@@ -122,11 +136,15 @@ public class EventAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
                     int count = eventListEnd.size();
                     eventList.removeAll(eventListEnd);
                     showEventListEnd = false;
+                    editor.putBoolean("showEventListEnd", false);
+                    editor.apply();
                     notifyItemRangeRemoved(startPosition - count, count);
                     notifyItemChanged(eventList.size());
                 } else {
                     eventList.addAll(eventListEnd);
                     showEventListEnd = true;
+                    editor.putBoolean("showEventListEnd", true);
+                    editor.apply();
                     notifyItemRangeInserted(startPosition, eventListEnd.size());
                     notifyItemChanged(eventList.size());
                 }
